1bd44efa5bd7bb3aa3f798ca51a96776b4133e5c
unknown <jeremy.tang@dawsoncollege.qc.ca>
2024-05-21 10:50:25 -0400

    Update MapReact.jsx to use state and listen for camera change event

--- src/components/MapReact.jsx
+++ src/components/MapReact.jsx
@@ -1,24 +1,32 @@
 import { API_KEY, MAP_ID } from "../config"
-import { useState } from "react";
-import { APIProvider, Map, AdvancedMarker, Pin, WindowInfo } from "@vis.gl/react-google-maps";
+import { useState, useCallback } from "react";
+import { APIProvider, Map, AdvancedMarker, Pin, WindowInfo, MapCameraChangedEvent } from "@vis.gl/react-google-maps";
+
+const INITIAL_CAMERA = {
+  center: { lat: 35.3241946, lng: 138.0303997 },
+  zoom: 8
+};
+const mainCities =
+{
+  Tokyo: { lat: 35.68152646705769, lng: 139.76838545853252 },
+  Osaka: { lat: 34.668609750638005, lng: 135.49808526112534 },
+  Kyoto: { lat: 35.011317032007454, lng: 135.7672398785833 }
+}
+
 
 export default function MapReact() {
-  const defCenter = { lat: 35.3241946, lng: 138.0303997 };
-  const mainCities = [
-    { name: "Tokyo", lat: 35.68152646705769, lng: 139.76838545853252 },
-    { name: "Osaka", lat: 34.668609750638005, lng: 135.49808526112534 },
-    { name: "Kyoto", lat: 35.011317032007454, lng: 135.7672398785833 },
-  ]
+  const [camera, setCamera] = useState(INITIAL_CAMERA);
+  const handleCameraChange = useCallback((e: MapCameraChangedEvent) => setCamera(e.detail), []);
 
   return (
     <APIProvider apiKey={API_KEY}>
       <div style={{ height: "100vh", width: "100%" }}>
-        <Map mapId={MAP_ID} defaultCenter={defCenter} defaultZoom={8}>
-          <AdvancedMarker position={mainCities[0]}>
+        <Map mapId={MAP_ID} {...camera} onCameraChanged={handleCameraChange}>
+          <AdvancedMarker position={mainCities.Tokyo}>
             {/* <Pin background={"red"}/> */}
           </AdvancedMarker>
-          <AdvancedMarker position={mainCities[1]}></AdvancedMarker>
-          <AdvancedMarker position={mainCities[2]}></AdvancedMarker>
+          <AdvancedMarker position={mainCities.Osaka}></AdvancedMarker>
+          <AdvancedMarker position={mainCities.Kyoto}></AdvancedMarker>
         </Map>
       </div>
     </APIProvider>
