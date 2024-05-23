e5d0e7de18171f8591d6ffa92c64215d66341893
Jeremy Tang <superspicynova@gmail.com>
2024-05-23 17:44:24 -0400

    Move data to constants.js file

---
+++ src/constants.js
@@ -0,0 +1,156 @@
+export const days = [
+  {
+    "emoji": "🍎",
+    "value": "August 10th",
+    "description": "Tokyo",
+    "activities": [
+      {
+        "value": "Arrive at Narita airport around 4pm (take either train/bus, check  price and time)"
+      },
+      {
+        "value": "Get to hotel in Asaku"
+      },
+      {
+        "value": "Exchange money at 7/11 get snacks"
+      },
+      {
+        "value": "Tokyo Station or Asakusa to eat"
+      }
+    ]
+  },
+  {
+    "emoji": "🍌",
+    "value": "August 11th",
+    "description":
+      "Naturally sweet and potassium-rich fruit. Bananas are a popular choice for their energy-boosting properties and can be enjoyed as a quick snack, added to smoothies, or used in baking."
+  },
+  {
+    "emoji": "🥦",
+    "value": "August 12th",
+    "description":
+      "Nutrient-packed green vegetable. Broccoli is packed with vitamins, minerals, and fiber. It has a distinct flavor and can be enjoyed steamed, roasted, or added to stir-fries."
+  },
+  {
+    "emoji": "🍎",
+    "value": "August 13th",
+    "description":
+      "Crisp and refreshing fruit. Apples are known for their versatility and nutritional benefits. They come in a variety of flavors and are great for snacking, baking, or adding to salads."
+  },
+  {
+    "emoji": "🍌",
+    "value": "August 14th",
+    "description":
+      "Naturally sweet and potassium-rich fruit. Bananas are a popular choice for their energy-boosting properties and can be enjoyed as a quick snack, added to smoothies, or used in baking."
+  },
+  {
+    "emoji": "🥦",
+    "value": "August 15th",
+    "description":
+      "Nutrient-packed green vegetable. Broccoli is packed with vitamins, minerals, and fiber. It has a distinct flavor and can be enjoyed steamed, roasted, or added to stir-fries."
+  },
+  {
+    "emoji": "🍎",
+    "value": "August 16th",
+    "description":
+      "Crisp and refreshing fruit. Apples are known for their versatility and nutritional benefits. They come in a variety of flavors and are great for snacking, baking, or adding to salads."
+  },
+  {
+    "emoji": "🍌",
+    "value": "August 17th",
+    "description":
+      "Naturally sweet and potassium-rich fruit. Bananas are a popular choice for their energy-boosting properties and can be enjoyed as a quick snack, added to smoothies, or used in baking."
+  },
+  {
+    "emoji": "🥦",
+    "value": "August 18th",
+    "description":
+      "Nutrient-packed green vegetable. Broccoli is packed with vitamins, minerals, and fiber. It has a distinct flavor and can be enjoyed steamed, roasted, or added to stir-fries."
+  },
+  {
+    "emoji": "🍎",
+    "value": "August 19th",
+    "description":
+      "Crisp and refreshing fruit. Apples are known for their versatility and nutritional benefits. They come in a variety of flavors and are great for snacking, baking, or adding to salads."
+  },
+  {
+    "emoji": "🍌",
+    "value": "August 20th",
+    "description":
+      "Naturally sweet and potassium-rich fruit. Bananas are a popular choice for their energy-boosting properties and can be enjoyed as a quick snack, added to smoothies, or used in baking."
+  },
+  {
+    "emoji": "🥦",
+    "value": "August 21st",
+    "description":
+      "Nutrient-packed green vegetable. Broccoli is packed with vitamins, minerals, and fiber. It has a distinct flavor and can be enjoyed steamed, roasted, or added to stir-fries."
+  },
+  {
+    "emoji": "🍎",
+    "value": "August 22th",
+    "description":
+      "Crisp and refreshing fruit. Apples are known for their versatility and nutritional benefits. They come in a variety of flavors and are great for snacking, baking, or adding to salads."
+  },
+  {
+    "emoji": "🍌",
+    "value": "August 23th",
+    "description":
+      "Naturally sweet and potassium-rich fruit. Bananas are a popular choice for their energy-boosting properties and can be enjoyed as a quick snack, added to smoothies, or used in baking."
+  },
+  {
+    "emoji": "🥦",
+    "value": "August 24th",
+    "description":
+      "Nutrient-packed green vegetable. Broccoli is packed with vitamins, minerals, and fiber. It has a distinct flavor and can be enjoyed steamed, roasted, or added to stir-fries."
+  },
+  {
+    "emoji": "🍎",
+    "value": "August 25th",
+    "description":
+      "Crisp and refreshing fruit. Apples are known for their versatility and nutritional benefits. They come in a variety of flavors and are great for snacking, baking, or adding to salads."
+  },
+  {
+    "emoji": "🍌",
+    "value": "August 26th",
+    "description":
+      "Naturally sweet and potassium-rich fruit. Bananas are a popular choice for their energy-boosting properties and can be enjoyed as a quick snack, added to smoothies, or used in baking."
+  },
+  {
+    "emoji": "🥦",
+    "value": "August 27th",
+    "description":
+      "Nutrient-packed green vegetable. Broccoli is packed with vitamins, minerals, and fiber. It has a distinct flavor and can be enjoyed steamed, roasted, or added to stir-fries."
+  },
+  {
+    "emoji": "🍎",
+    "value": "August 28th",
+    "description":
+      "Crisp and refreshing fruit. Apples are known for their versatility and nutritional benefits. They come in a variety of flavors and are great for snacking, baking, or adding to salads."
+  },
+  {
+    "emoji": "🍌",
+    "value": "August 29th",
+    "description":
+      "Naturally sweet and potassium-rich fruit. Bananas are a popular choice for their energy-boosting properties and can be enjoyed as a quick snack, added to smoothies, or used in baking."
+  },
+  {
+    "emoji": "🥦",
+    "value": "August 30th",
+    "description":
+      "Nutrient-packed green vegetable. Broccoli is packed with vitamins, minerals, and fiber. It has a distinct flavor and can be enjoyed steamed, roasted, or added to stir-fries."
+  },
+  {
+    "emoji": "🍎",
+    "value": "August 31st",
+    "description":
+      "Crisp and refreshing fruit. Apples are known for their versatility and nutritional benefits. They come in a variety of flavors and are great for snacking, baking, or adding to salads."
+  },
+  {
+    "emoji": "🍌",
+    "value": "September 1st",
+    "description":
+      "Naturally sweet and potassium-rich fruit. Bananas are a popular choice for their energy-boosting properties and can be enjoyed as a quick snack, added to smoothies, or used in baking."
+  }
+];
+
+// const activities, move them outside of days
+// const events = max arrive, kevin leave, ...
