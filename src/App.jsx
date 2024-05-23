0b633fc608864ef58abe0516f67b5818a6a21a39
Jeremy Tang <superspicynova@gmail.com>
2024-05-23 00:27:44 -0400

    Add mantine.dev to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css';
+import '@mantine/core/styles.css';
+import { MantineProvider } from '@mantine/core';
 import MapReact from './components/MapReact';
+import DrawerReact from './components/DrawerReact';
 
 export default function App() {
 
   return (
-    <div className="App">
-      <MapReact/>
-    </div>
+    <MantineProvider>
+      <div className="App">
+        {/* <DrawerReact /> */}
+        {/* <MapReact /> */}
+      </div>
+    </MantineProvider>
   );
 }
