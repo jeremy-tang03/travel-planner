8e64ba03e9fc4abccde1644f085f73be3b5934be
Jeremy Tang <superspicynova@gmail.com>
2024-06-13 04:15:32 -0400

    Implement new marker clustering feature for map

--- src/components/MapReact.jsx
+++ src/components/MapReact.jsx
@@ -4,6 +4,7 @@ import {
   APIProvider, Map, AdvancedMarker, Pin, MapCameraChangedEvent,
   useMapsLibrary, useMap, ControlPosition, MapControl
 } from "@vis.gl/react-google-maps";
+import { MarkerClusterer } from '@googlemaps/markerclusterer';
 import { getKey } from "../helper";
 import { TextInput } from '@mantine/core';
 
@@ -65,21 +66,83 @@ const MapHandler = ({ place, setCurrentMarker }) => {
   return null;
 };
 
-export default function MapReact({ pw, data, isPC }) {
-  const INITIAL_CAMERA = {
-    center: { lat: 35.3241946, lng: 138.0303997 },
-    zoom: (isPC ? 8 : 6.3)
+const Markers = ({ points }) => {
+  const map = useMap();
+  const [markers, setMarkers] = useState({});
+  const clusterer = useRef(null);
+
+  const renderer = {
+    render: ({ count, position }) =>
+      // eslint-disable-next-line no-undef
+      new google.maps.Marker({
+        label: { text: String(count), color: "white", fontSize: "10px" },
+        position,
+        // adjust zIndex to be above other markers
+        // eslint-disable-next-line no-undef
+        zIndex: Number(google.maps.Marker.MAX_ZINDEX) + count,
+      }),
   };
+
+  // Initialize MarkerClusterer
+  useEffect(() => {
+    if (!map) return;
+    if (!clusterer.current) {
+      clusterer.current = new MarkerClusterer({ map, renderer });
+    }
+  }, [map]);
+
+  // Update markers
+  useEffect(() => {
+    clusterer.current?.clearMarkers();
+    clusterer.current?.addMarkers(Object.values(markers));
+  }, [markers]);
+
+  const setMarkerRef = (marker, key) => {
+    if (marker && markers[key]) return;
+    if (!marker && !markers[key]) return;
+
+    setMarkers(prev => {
+      if (marker) {
+        return { ...prev, [key]: marker };
+      } else {
+        const newMarkers = { ...prev };
+        delete newMarkers[key];
+        return newMarkers;
+      }
+    });
+  };
+
+  return (
+    <>
+      {points.map(point => (
+        <AdvancedMarker
+          position={point}
+          key={point.key}
+          ref={marker => setMarkerRef(marker, point.key)}>
+          {/* <span className="tree">🌳</span> */}
+          <Pin background={"red"} />
+        </AdvancedMarker>
+      ))}
+    </>
+  );
+};
+
+export default function MapReact({ pw, data, isPC }) {
   const mainCities = [
-    { "name": "Tokyo", position: { lat: 35.68152646705769, lng: 139.76838545853252 } },
-    { "name": "Osaka", position: { lat: 34.668609750638005, lng: 135.49808526112534 } },
-    { "name": "Kyoto", position: { lat: 35.011317032007454, lng: 135.7672398785833 } }
+    {
+      name: "Tokyo", lat: 35.68152646705769, lng: 139.76838545853252,
+      key: JSON.stringify({ name: "Tokyo", lat: 35.68152646705769, lng: 139.76838545853252 })
+    },
+    {
+      name: "Osaka", lat: 34.668609750638005, lng: 135.49808526112534,
+      key: JSON.stringify({ name: "Osaka", lat: 34.668609750638005, lng: 135.49808526112534 })
+    },
+    {
+      name: "Kyoto", lat: 35.011317032007454, lng: 135.7672398785833,
+      key: JSON.stringify({ name: "Kyoto", lat: 35.011317032007454, lng: 135.7672398785833 })
+    }
   ]
   const [mapReady, setMapReady] = useState(false);
-  const [camera, setCamera] = useState(INITIAL_CAMERA);
-  const handleCameraChange = useCallback((e: MapCameraChangedEvent) => setCamera(e.detail), []);
-  const citiesMarker = Object.values(mainCities).map(
-    c => <AdvancedMarker key={c.name} position={c.position}><Pin background={"red"} /></AdvancedMarker>);
   const [apiKey, setApiKey] = useState(getKey(pw));
   const [selectedPlace, setSelectedPlace] = useState(null);
   const [currentMarker, setCurrentMarker] = useState(null);
@@ -93,8 +156,8 @@ export default function MapReact({ pw, data, isPC }) {
       {mapReady ?
         <APIProvider apiKey={apiKey} solutionChannel="" >
           <div style={{ height: "100vh", width: "100%" }}>
-            <Map mapId={MAP_ID} {...camera} onCameraChanged={handleCameraChange} style={{ zIndex: 10 }} disableDefaultUI={true}>
-              {citiesMarker}
+            <Map mapId={MAP_ID} defaultCenter={{ lat: 35.3241946, lng: 138.0303997 }} defaultZoom={(isPC ? 8 : 6.3)} style={{ zIndex: 10 }} disableDefaultUI={true}>
+              <Markers points={mainCities} />
               {currentMarker}
             </Map>
             <CustomMapControl
